5d1275b682bdc45ab84d18d4298a79e159f327fd
Swizaesar Reza <swizaesar.worker@gmail.com>
2021-01-25 22:32:01 +0700

    change name variable sidebar

--- src/Components/Sidebar/style.js
+++ src/Components/Sidebar/style.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
 import { Navbar } from "reactstrap";
-import { sidebarWidth } from "../../Utils/Variable";
 const Style = styled(Navbar)`
     position: fixed;
     display: block;
@@ -12,7 +11,7 @@ const Style = styled(Navbar)`
     bottom: 0;
     height: 100%;
     background: #33383e;
-    width: ${sidebarWidth.width}px;
+    width: 250px;
     .navbar-heading {
         padding-left: 10px;
     }
